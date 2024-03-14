286550f8506ecf1468831a4a088d9cf6f301d8bc
Kader2637 <130645424+Kader2637@users.noreply.github.com>
2024-03-14 16:09:59 +0700

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -49,22 +49,6 @@
 <link rel="preconnect" href="https://fonts.gstatic.com/" crossorigin>
 <link href="https://fonts.googleapis.com/css2?family=Public+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&amp;display=swap" rel="stylesheet">
 
-<link rel="stylesheet" href="{{ asset('asset-login/assets/vendor/fonts/tabler-iconsea04.css?id=6ad8bc28559d005d792d577cf02a2116') }}" />
-<link rel="stylesheet" href="{{ asset('asset-login/assets/vendor/fonts/fontawesome8a69.css?id=a2997cb6a1c98cc3c85f4c99cdea95b5') }}" />
-<link rel="stylesheet" href="{{ asset('asset-login/assets/vendor/fonts/flag-icons80a8.css?id=121bcc3078c6c2f608037fb9ca8bce8d') }}" />
-<!-- Core CSS -->
-<link rel="stylesheet" href="{{ asset('asset-login/assets/vendor/css/rtl/core6cc1.css?id=9dd8321ea008145745a7d78e072a6e36') }}" class="template-customizer-core-css" />
-<link rel="stylesheet" href="{{ asset('asset-login/assets/vendor/css/rtl/theme-defaultfc79.css?id=a4539ede8fbe0ee4ea3a81f2c89f07d9') }}" class="template-customizer-theme-css" />
-<link rel="stylesheet" href="{{ asset('asset-login/assets/css/demof1ed.css?id=ddd2feb83a604f9e432cdcb29815ed44') }}" />
-<link rel="stylesheet" href="{{ asset('asset-login/assets/vendor/libs/node-waves/node-wavesd178.css?id=aa72fb97dfa8e932ba88c8a3c04641bc') }}" />
-<link rel="stylesheet" href="{{ asset('asset-login/assets/vendor/libs/perfect-scrollbar/perfect-scrollbar7358.css?id=280196ccb54c8ae7e29ea06932c9a4b6') }}" />
-<link rel="stylesheet" href="{{ asset('asset-login/assets/vendor/libs/typeahead-js/typeaheadb5e1.css?id=2603197f6b29a6654cb700bd9367e2a3') }}" />
-
-<!-- Vendor Styles -->
-<!-- Vendor -->
-<link rel="stylesheet" href="{{ asset('asset-login/assets/vendor/libs/%40form-validation/umd/styles/index.min.css') }}" />
-
-
 <!-- Page Styles -->
 <!-- Page -->
 <link rel="stylesheet" href="{{ asset('asset-login/assets/vendor/css/pages/page-auth.css') }}">
