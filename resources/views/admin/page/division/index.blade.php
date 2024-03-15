33d958f2705f19c78be054c5485210efce3f185b
Amir Zuhdi Wibowo <cakadi190@gmail.com>
2024-03-15 14:55:03 +0700

    Fix: Modal input not exist on Division CRUD [Done]

--- resources/views/admin/page/division/index.blade.php
+++ resources/views/admin/page/division/index.blade.php
@@ -1,143 +1,142 @@
 @extends('admin.layouts.app')
 @section('content')
-
-<div class="card">
-    <div class="card-body">
-        <div class="row g-2">
-            <div class="col-sm-4">
-                <h3 class="mx-5">Divisi</h3>
-            </div>
-            <div class="col-sm-auto ms-auto d-flex">
-                <div class="search-box mx-3">
-                    <input type="text" class="form-control" id="searchMemberList" placeholder="Cari Siswa...">
-                    <i class="ri-search-line search-icon"></i>
+    <div class="card">
+        <div class="card-body">
+            <div class="row g-2">
+                <div class="col-sm-4">
+                    <h3 class="mx-5">Divisi</h3>
                 </div>
-                <div class="list-grid-nav hstack gap-1">
-                    <button class="btn btn-success" data-bs-toggle="modal" data-bs-target="#add">
-                        Tambah Data
-                    </button>
+                <div class="col-sm-auto ms-auto d-flex">
+                    <div class="search-box mx-3">
+                        <input type="text" class="form-control" id="searchMemberList" placeholder="Cari Siswa...">
+                        <i class="ri-search-line search-icon"></i>
+                    </div>
+                    <div class="list-grid-nav hstack gap-1">
+                        <button class="btn btn-success" data-bs-toggle="modal" data-bs-target="#add">
+                            Tambah Data
+                        </button>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
-<div class="row">
-    @forelse ($divisions as $division)
-        <div class="col-lg-3">
-            <div class="card card-height-100">
-                <div class="card-body">
-                    <div class="d-flex justify-content-between">
-                        <div>
-                            <p class="fw-medium text-muted mb-0">{{ $division->name }}</p>
-                            <h2 class="mt-4 ff-secondary fw-bold">
-                                <span class="counter-value" data-target="50"></span>
-                            </h2>
+    <div class="row">
+        @forelse ($divisions as $division)
+            <div class="col-lg-3">
+                {{-- modal edit division start --}}
+                <div class="modal fade" id="modal-edit-{{ $division->id }}" tabindex="-1" aria-labelledby="editDataDivisi"
+                    aria-hidden="true">
+                    <div class="modal-dialog">
+                        <div class="modal-content">
+                            <div class="modal-header">
+                                <h5 class="modal-title" id="editDataDivisi">Edit Divisi</h5>
+                                <button type="button" class="btn-close" data-bs-dismiss="modal"
+                                    aria-label="Close"></button>
+                            </div>
+                            <form method="POST" id="form-update">
+                                @csrf
+                                @method('PATCH')
+                                <div class="modal-body">
+                                    <div class="mb-1">
+                                        <label for="name" class="col-form-label">Divisi</label>
+                                        <input type="text" value="{{ $division->name }}" class="form-control @error('name') is-invalid @enderror"
+                                            id="name-edit" name="name" placeholder="Mis: Website, Mobile, dan lain-lain" />
+                                        @error('name')
+                                            <p class="text-danger">
+                                                {{ $message }}
+                                            </p>
+                                        @enderror
+                                    </div>
+                                </div>
+                                <div class="modal-footer">
+                                    <button type="submit" class="btn btn-primary">Simpan</button>
+                                    <button type="button" class="btn btn-light" data-bs-dismiss="modal">Tutup</button>
+                                </div>
+                            </form>
                         </div>
-                        <div>
-                            <div class="avatar-sm flex-shrink-0">
-                                <span class="avatar-title bg-success-subtle rounded-circle fs-2">
+                    </div>
+                </div>
+                {{-- modal edit division end --}}
 
-                                </span>
+                <div class="card card-height-100">
+                    <div class="card-body">
+                        <div class="d-flex justify-content-between">
+                            <div>
+                                <p class="fw-medium text-muted mb-0">{{ $division->name }}</p>
+                                <h2 class="mt-4 ff-secondary fw-bold">
+                                    <span class="counter-value" data-target="50"></span>
+                                </h2>
+                            </div>
+                            <div>
+                                <div class="avatar-sm flex-shrink-0">
+                                    <span class="avatar-title bg-success-subtle rounded-circle fs-2">
+
+                                    </span>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="d-flex justify-content-between">
+                            <p class="mb-0 text-muted">
+                                {{ \Carbon\Carbon::parse($division->created_at)->locale('id_ID')->isoFormat('D MMMM YYYY') }}
+                            </p>
+                            <div class="gap-2">
+                                <button class="bg-transparent border-0">
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20"
+                                        viewBox="0 0 20 20" fill="none">
+                                        <path
+                                            d="M15 4H20V6H18V19C18 19.5523 17.5523 20 17 20H3C2.44772 20 2 19.5523 2 19V6H0V4H5V1C5 0.44772 5.44772 0 6 0H14C14.5523 0 15 0.44772 15 1V4ZM7 9V15H9V9H7ZM11 9V15H13V9H11ZM7 2V4H13V2H7Z"
+                                            fill="#DC3545" />
+                                    </svg>
+                                </button>
+                                <button class="bg-transparent border-0 btn-edit" data-bs-toggle="modal"
+                                    data-bs-target="#modal-edit-{{ $division->id }}">
+                                    <svg width="19" height="19" viewBox="0 0 19 19" fill="none"
+                                        xmlns="http://www.w3.org/2000/svg">
+                                        <path
+                                            d="M4 5H3C2.46957 5 1.96086 5.21071 1.58579 5.58579C1.21071 5.96086 1 6.46957 1 7V16C1 16.5304 1.21071 17.0391 1.58579 17.4142C1.96086 17.7893 2.46957 18 3 18H12C12.5304 18 13.0391 17.7893 13.4142 17.4142C13.7893 17.0391 14 16.5304 14 16V15M13 3L16 6M17.385 4.58511C17.7788 4.19126 18.0001 3.65709 18.0001 3.10011C18.0001 2.54312 17.7788 2.00895 17.385 1.61511C16.9912 1.22126 16.457 1 15.9 1C15.343 1 14.8088 1.22126 14.415 1.61511L6 10.0001V13.0001H9L17.385 4.58511Z"
+                                            stroke="#FFAE1F" stroke-width="2" stroke-linecap="round"
+                                            stroke-linejoin="round" />
+                                    </svg>
+                                </button>
                             </div>
                         </div>
                     </div>
-                    <div class="d-flex justify-content-between">
-                        <p class="mb-0 text-muted">
-                            {{ \Carbon\Carbon::parse($division->created_at)->locale('id_ID')->isoFormat('D MMMM YYYY') }}
-                        </p>
-                        <div class="gap-2">
-                            <button class="bg-transparent border-0">
-                                <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none">
-                                <path d="M15 4H20V6H18V19C18 19.5523 17.5523 20 17 20H3C2.44772 20 2 19.5523 2 19V6H0V4H5V1C5 0.44772 5.44772 0 6 0H14C14.5523 0 15 0.44772 15 1V4ZM7 9V15H9V9H7ZM11 9V15H13V9H11ZM7 2V4H13V2H7Z" fill="#DC3545"/>
-                                </svg>
-                            </button>
-                            <button class="bg-transparent border-0 btn-edit" data-bs-toggle="modal" data-bs-target="#modal-edit" data-id="{{ $division->id }}" data-name="{{ $division->name }}">
-                                <svg width="19" height="19" viewBox="0 0 19 19" fill="none" xmlns="http://www.w3.org/2000/svg">
-                                <path d="M4 5H3C2.46957 5 1.96086 5.21071 1.58579 5.58579C1.21071 5.96086 1 6.46957 1 7V16C1 16.5304 1.21071 17.0391 1.58579 17.4142C1.96086 17.7893 2.46957 18 3 18H12C12.5304 18 13.0391 17.7893 13.4142 17.4142C13.7893 17.0391 14 16.5304 14 16V15M13 3L16 6M17.385 4.58511C17.7788 4.19126 18.0001 3.65709 18.0001 3.10011C18.0001 2.54312 17.7788 2.00895 17.385 1.61511C16.9912 1.22126 16.457 1 15.9 1C15.343 1 14.8088 1.22126 14.415 1.61511L6 10.0001V13.0001H9L17.385 4.58511Z" stroke="#FFAE1F" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
-                                </svg>
-                            </button>
+                </div>
+            </div>
+        @empty
+            {{--  --}}
+        @endforelse
+    </div>
+
+    {{-- modal add division start --}}
+    <div class="modal fade" id="add" tabindex="-1" aria-labelledby="varyingcontentModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="varyingcontentModalLabel">Tambah Divisi</h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                </div>
+                <form action="{{ route('division.store') }}" method="POST">
+                    @csrf
+                    <div class="modal-body">
+                        <div class="mb-1">
+                            <label for="name" class="col-form-label">Divisi</label>
+                            <input type="text" class="form-control @error('name') is-invalid @enderror" id="name"
+                                name="name" placeholder="Mis: Website, Mobile, dan lain-lain" />
+                            @error('name')
+                                <p class="text-danger">
+                                    {{ $message }}
+                                </p>
+                            @enderror
                         </div>
                     </div>
-                </div>
-            </div>
-        </div>
-    @empty
-        
-    @endforelse
-</div>
-
-{{-- modal add division start --}}
-<div class="modal fade" id="add" tabindex="-1" aria-labelledby="varyingcontentModalLabel" aria-hidden="true">
-    <div class="modal-dialog">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="varyingcontentModalLabel">Tambah Divisi</h5>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <form action="{{ route('division.store') }}" method="POST">
-                @csrf
-                <div class="modal-body">
-                    <div class="mb-1">
-                        <label for="name" class="col-form-label">Divisi</label>
-                        <input type="text" class="form-control @error('name') is-invalid @enderror" id="name" name="name" placeholder="Tambah Divisi">
-                        @error('name')
-                            <p class="text-danger">
-                                {{ $message }}
-                            </p>
-                        @enderror
+                    <div class="modal-footer">
+                        <button type="submit" class="btn btn-primary">Buat</button>
+                        <button type="button" class="btn btn-light" data-bs-dismiss="modal">Tutup</button>
                     </div>
-                </div>
-                <div class="modal-footer">
-                    <button type="submit" class="btn btn-primary">Buat</button>
-                    <button type="button" class="btn btn-light" data-bs-dismiss="modal">Tutup</button>
-                </div>
-            </form>
+                </form>
+            </div>
         </div>
     </div>
-</div>
-{{-- modal add division end --}}
-
-{{-- modal edit division start --}}
-<div class="modal fade" id="modal-edit" tabindex="-1" aria-labelledby="varyingcontentModalLabel" aria-hidden="true">
-    <div class="modal-dialog">
-        <div class="modal-content">
-            <div class="modal-header">
-                <h5 class="modal-title" id="varyingcontentModalLabel">Edit Divisi</h5>
-                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-            </div>
-            <form method="POST" id="form-update">
-                @csrf
-                @method('PATCH')
-                <div class="modal-body">
-                    <div class="mb-1">
-                        <label for="name" class="col-form-label">Divisi</label>
-                        <input type="text" class="form-control @error('name') is-invalid @enderror" id="name-edit" name="name" placeholder="Tambah Divisi">
-                        @error('name')
-                            <p class="text-danger">
-                                {{ $message }}
-                            </p>
-                        @enderror
-                    </div>
-                </div>
-                <div class="modal-footer">
-                    <button type="submit" class="btn btn-primary">Simpan</button>
-                    <button type="button" class="btn btn-light" data-bs-dismiss="modal">Tutup</button>
-                </div>
-            </form>
-        </div>
-    </div>
-</div>
-{{-- modal edit division end --}}
-
-@endsection
-@section('script')
-    <script>
-        $('.btn-edit').click(function () {
-            var id = $(this).data('id'); 
-            var name = $(this).data('name'); 
-            $('#form-update').attr('action', '/division/' + id);
-            $('#name-edit').val(name);
-            $('#modal-edit').modal('show');
-        });
-    </script>
+    {{-- modal add division end --}}
 @endsection
